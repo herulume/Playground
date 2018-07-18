c74ef8a60f67d1853deb1988fe625fa652087eb7
herulume <herulume@protonmail.com>
2018-07-18 16:35:33 +0100

    IOMonad questions

---
+++ IOMonad.hs
@@ -0,0 +1,7 @@
+dialogo :: String -> IO String
+dialogo s = putStr s >> getLine >>= return
+
+questionario :: [String] -> IO [String]
+questionario [] = return [];
+questionario (q:qs) = dialogo q >>= (\x -> (questionario qs >>= (\y -> return (x:y))) )
+
