e1c453a337cced265e7876d9c3b18a1f9e396c47
herulume <herulume@protonmail.com>
2018-07-23 19:01:27 +0100

    Add a second carrier type

--- FixedPoints/AST.hs
+++ FixedPoints/AST.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE DeriveFunctor #-}
-
 import Data.Functor.Foldable 
 
 data Op = Add | Mult deriving Show
 data ASTF r = BinOpF Op r r | Num Int 
+type AST = Fix ASTF 
 
--- check if this is right
 instance Functor ASTF where
     fmap f (BinOpF op e d) = BinOpF op (f e) (f d)
     fmap _ (Num a) = (Num a)  
@@ -13,10 +11,17 @@ instance Functor ASTF where
 simpleExprFix :: Fix ASTF
 simpleExprFix = Fix (BinOpF Mult (Fix (BinOpF Add (Fix (Num 1)) (Fix (Num 2)))) (Fix (Num 3)))
 
+interpret :: Either AST AST -> Either Int String
+interpret (Left ast) = Left $ cata algebraI ast
+interpret (Right ast) = Right $ cata algebraS ast
 
-interpret :: Fix ASTF -> Int
-interpret = cata algebra where
-    algebra :: ASTF Int -> Int
-    algebra (Num n) = n
-    algebra (BinOpF Add a b) = a + b
-    algebra (BinOpF Mult a b) = a * b
+algebraI :: ASTF Int -> Int
+algebraI (Num n) = n
+algebraI (BinOpF Add a b) = a + b
+algebraI (BinOpF Mult a b) = a * b
+
+algebraS :: ASTF String -> String
+algebraS (Num n) = show n
+algebraS (BinOpF Add a b) = "(" ++ a ++ " + " ++  b ++ ")"
+algebraS (BinOpF Mult a b) = a ++ " * " ++   b
+
