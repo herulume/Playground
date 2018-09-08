92552425bd0b8a57d239e35259277f8b5bacf340
herulume <herulume@protonmail.com>
2018-09-08 15:55:46 +0100

    m

--- typeSafeVectors.hs
+++ typeSafeVectors.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE GADTs #-} --extra type information 
 {-# LANGUAGE KindSignatures #-} -- kinds in signatures
 {-# LANGUAGE TypeFamilies #-} -- functions that operate on types
---{-# LANGUAGE UndecidableInstances #-} -- Add type family 
 
 data Nat where
     Zero :: Nat
@@ -10,19 +9,22 @@ data Nat where
 
 data Vector (n :: Nat) (a :: *) where
     VNil  :: Vector 'Zero a
-    VCons :: a -> Vector n a -> Vector ('Succ n) a
+    (:-) :: a -> Vector n a -> Vector ('Succ n) a
+infixr 5 :-
 
 instance Show a => Show (Vector n a) where
     show VNil = "VNil"
-    show (VCons a as) = "VCons " ++ show a ++ " (" ++ show as ++ ")"
+    show (a :- as) = show a ++ " :- " ++ show as 
 
-type family Add n m where
+type family Add (n :: Nat) (m :: Nat)  where
     Add 'Zero n = n
-    -- Add ('Succ n) m = Add n ('Succ m) -- GHC can't infer this is finite so we need UndecidableInstances
     Add ('Succ n) m = 'Succ (Add n m)
 
 
 -- api 
-append :: Vector n a -> Vector m a -> Vector (Add n m) a
-append VNil rest = rest
-append (VCons a as) rest = VCons a $ append as rest 
+vappend :: Vector n a -> Vector m a -> Vector (Add n m) a
+vappend VNil rest = rest
+vappend (a :- as) rest = a :- vappend as rest 
+
+vhead :: Vector (Succ n) a -> a
+vhead (a :-  _ ) = a
