f65ae99e492faac673590ac81a5fddfe24d1e8c0
herulume <herulume@protonmail.com>
2019-10-29 23:17:27 +0000

    m

--- Haskell/AskHell/Monads/IOMonad.hs
+++ Haskell/AskHell/Monads/IOMonad.hs
@@ -3,5 +3,5 @@ dialogo s = putStr s >> getLine >>= return
 
 questionario :: [String] -> IO [String]
 questionario [] = return [];
-questionario (q:qs) = dialogo q >>= (\x -> (questionario qs >>= (\y -> return (x:y))) )
+questionario (q:qs) = dialogo q >>= \x -> questionario qs >>= (\y -> return (x:y))
 
