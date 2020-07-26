ca42c3e0869fb15a718c14bb3159b463f1e03cf2
herulume <herulume@protonmail.com>
2020-07-26 14:47:29 +0100

    Add parallel testing

---
+++ Go/learn-tdd/strcuts/v3/shapes_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+        t.Parallel() // mark to run in parallel
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		tt := tt // NOTE: https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel() // marks each test case as capable of running in parallel with each other
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+
+}
