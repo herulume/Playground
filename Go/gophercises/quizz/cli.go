a89558b53a463a9a64e6af50af2c8f0c7d73d0f5
herulume <herulume@protonmail.com>
2020-08-17 22:29:10 +0100

    add flags

--- Go/gophercises/quizz/cli.go
+++ Go/gophercises/quizz/cli.go
@@ -7,28 +7,28 @@ import (
 )
 
 type Cli struct {
-	quizz Game
+	game Game
 	out   io.Writer
 }
 
-func NewCLI(quizzLocation string) (*Cli, error) {
-	quizz, err := NewQuizz(quizzLocation)
+func NewCLI(gameLocation string) (*Cli, error) {
+	game, err := NewQuizz(gameLocation)
 
 	if err != nil {
-		return nil, fmt.Errorf("Error creating CLI for the quizz %v", err)
+		return nil, fmt.Errorf("Error creating CLI for the game %v", err)
 	}
 
-	cli := Cli{quizz, os.Stdout}
+	cli := Cli{game, os.Stdout}
 
 	return &cli, nil
 }
 
-const loadingPrompt = "Loading default quizz..."
+const loadingPrompt = "Loading default game..."
 
 func (c *Cli) Play() {
 	fmt.Fprintln(c.out, loadingPrompt)
 
-	c.quizz.Start(c.out)
+	c.game.Start(c.out)
 
-	c.quizz.Finish(c.out)
+	c.game.Finish(c.out)
 }
--- Go/gophercises/quizz/cmd/cli/main.go
+++ Go/gophercises/quizz/cmd/cli/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/herulume/quizz"
 )
 
 func main() {
-	q, err := quizz.NewCLI("problems.csv")
+	csv := flag.String("file", "problems.csv", "File to load the quizz from")
+	flag.Parse()
+
+	q, err := quizz.NewCLI(*csv)
 
 	if err != nil {
 		fmt.Printf("Failed to create the quizz %v", err)
