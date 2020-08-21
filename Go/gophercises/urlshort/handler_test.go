c3a63e2fb76630da4bbe5a79323e0571d1538a2a
herulume <herulume@protonmail.com>
2020-08-21 12:03:26 +0100

    Add redirects

--- Go/gophercises/urlshort/handler_test.go
+++ Go/gophercises/urlshort/handler_test.go
@@ -9,7 +9,9 @@ import (
 func TestMapHandler(t *testing.T) {
 
 	t.Run("it uses the fallback for unknown routes", func(t *testing.T) {
-		mapHandler := createEmptyMapHandler()
+		noPath := map[string]string{}
+		mapHandler := createMapHandlerWithDefault(noPath)
+
 		request, _ := http.NewRequest(http.MethodGet, "/unknown", nil)
 		response := httptest.NewRecorder()
 
@@ -22,10 +24,48 @@ func TestMapHandler(t *testing.T) {
 				fallbackResponse, got)
 		}
 	})
+
+	t.Run("it routes known routes", func(t *testing.T) {
+		path := "/test"
+		site := "https://test.com"
+		knownPath := map[string]string{
+			path: site,
+		}
+		mapHandler := createMapHandlerWithDefault(knownPath)
+
+		request, _ := http.NewRequest(http.MethodGet, path, nil)
+		response := httptest.NewRecorder()
+
+		mapHandler(response, request)
+		result := response.Result()
+
+		assertStatus(t, result, http.StatusFound)
+		assertURL(t, result, site)
+	})
 }
 
-func createEmptyMapHandler() http.HandlerFunc {
-	pathToUrls := map[string]string{}
+func createMapHandlerWithDefault(paths map[string]string) http.HandlerFunc {
 	fallbackHandler := http.HandlerFunc(fallback)
-	return MapHandler(pathToUrls, fallbackHandler)
+	return MapHandler(paths, fallbackHandler)
+}
+
+func assertURL(t *testing.T, resp *http.Response, want string) {
+	t.Helper()
+	url, err := resp.Location()
+
+	if err != nil {
+		t.Fatal("Could not read location", err)
+	}
+
+	if url.String() != want {
+		t.Errorf("Expected url to be %s, got %s", url, want)
+	}
+}
+
+func assertStatus(t *testing.T, resp *http.Response, want int) {
+	t.Helper()
+	if resp.StatusCode != want {
+		t.Errorf("Expected status to be %d, got %d",
+			want, resp.StatusCode)
+	}
 }
