911cf24d5301e2453bee3900fa8c1bf9959af229
herulume <herulume@protonmail.com>
2020-08-23 21:48:50 +0100

    u

--- Go/gophercises/urlshort/cmd/main.go
+++ Go/gophercises/urlshort/cmd/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/herulume/urlshort"
 )
 
 func main() {
 	mux := defaultMux()
+
+	var yaml = flag.String("yaml", "urls.yaml", "urls in yaml format")
+	flag.Parse()
+
+	yamlFile, err := os.Open(*yaml)
+	defer yamlFile.Close()
+
+	if err != nil {
+		panic(err)
+	}
 
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
@@ -17,18 +30,12 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler(getFileBytes(yamlFile), mapHandler)
+
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
 }
@@ -37,6 +44,18 @@ func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	return mux
+}
+
+func getFileBytes(f *os.File) []byte {
+	buf := bytes.Buffer{}
+
+	_, err := buf.ReadFrom(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return buf.Bytes()
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
